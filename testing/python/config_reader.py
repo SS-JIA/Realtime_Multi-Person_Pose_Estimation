c529861919aa26488e93dfee277b2ddcecf55fcc
jiasicheng614@gmail.com <jiasicheng614@gmail.com>
2018-02-24 00:57:21 -0500

    Make paths absolute in config_reader

--- testing/python/config_reader.py
+++ testing/python/config_reader.py
@@ -1,9 +1,11 @@
 from configobj import ConfigObj
 import numpy as np
+import os.path
 
+base_path = os.path.dirname(os.path.abspath(__file__))
 
-def config_reader():
-    config = ConfigObj('config')
+def config_reader(path):
+    config = ConfigObj(path)
 
     param = config['param']
     model_id = param['modelID']
@@ -11,6 +13,8 @@ def config_reader():
     model['boxsize'] = int(model['boxsize'])
     model['stride'] = int(model['stride'])
     model['padValue'] = int(model['padValue'])
+    model['deployFile'] = os.path.join(base_path, model['deployFile'])
+    model['caffemodel'] = os.path.join(base_path, model['caffemodel'])
     #param['starting_range'] = float(param['starting_range'])
     #param['ending_range'] = float(param['ending_range'])
     param['octave'] = int(param['octave'])
@@ -28,6 +32,3 @@ def config_reader():
     param['GPUdeviceNumber'] = int(param['GPUdeviceNumber'])
 
     return param, model
-
-if __name__ == "__main__":
-    config_reader()
